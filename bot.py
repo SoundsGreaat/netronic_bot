508e8feec367c82686246d8112befb1fcfded5fa
SoundsGreaat <imithos321@gmail.com>
2024-03-08 18:28:17 +0200

    added openai assistant

--- bot.py
+++ bot.py
@@ -3,12 +3,12 @@ import re
 import threading
 import asyncio
 import time
-from collections import defaultdict
-
-from time import sleep
-
 import gforms
+
+from collections import defaultdict
+from time import sleep
 from telebot import TeleBot, types, apihelper
+from openai import OpenAI
 
 from google_forms_filler import FormFiller
 from database import DatabaseConnection, test_connection, update_authorized_users, find_contact_by_name
@@ -38,6 +38,8 @@ edit_link_data = {
 add_link_data = defaultdict(dict)
 
 add_employee_data = defaultdict(dict)
+
+openai_data = defaultdict(dict)
 
 process_in_progress = {}
 
@@ -114,6 +116,8 @@ def delete_messages(chat_id, dict_key='messages_to_delete'):
             pass
 
 
+client = OpenAI()
+assistant_id = os.getenv('OPENAI_ASSISTANT_ID')
 bot = TeleBot(os.getenv('NETRONIC_BOT_TOKEN'))
 
 main_menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -228,7 +232,9 @@ def add_link(call):
     markup = types.InlineKeyboardMarkup()
     markup.add(cancel_btn)
     bot.delete_message(call.message.chat.id, call.message.message_id)
-    sent_message = bot.send_message(call.message.chat.id, '📝 Введіть назву нового посилання:', reply_markup=markup)
+    sent_message = bot.send_message(call.message.chat.id,
+                                    '📝 Введіть назву нового посилання (бажано на початку додати емодзі):',
+                                    reply_markup=markup)
     add_link_data[call.message.chat.id]['saved_message'] = sent_message
     add_link_data[call.message.chat.id]['link_type_id'] = link_type_id
 
@@ -315,7 +321,7 @@ def edit_link(call):
     bot.delete_message(call.message.chat.id, call.message.message_id)
     if operation == 'name':
         edit_link_data['column'][call.message.chat.id] = ('name', link_id)
-        message_text = f'📝 Введіть нову назву для посилання <b>{link_name}</b>:'
+        message_text = f'📝 Введіть нову назву для посилання <b>{link_name}</b> (бажано на початку додати емодзі):'
     else:
         edit_link_data['column'][call.message.chat.id] = ('link', link_id)
         message_text = f'🔗 Введіть нове посилання для <b>{link_name}</b>:'
@@ -956,11 +962,69 @@ def back_to_send_contacts_menu(call):
 
 @bot.message_handler(func=lambda message: message.text == '💭 Маєш питання?')
 @authorized_only(user_type='users')
-def send_support_form(message):
-    send_question_form(message,
-                       'https://docs.google.com/forms/d/e/1FAIpQLSfzamHCZtyBu2FDI3dYlV8PZw46ON2qzhTGrIRqA9eFAiI86Q/'
-                       'viewform',
-                       delete_previous_message=True)
+def ai_question(message):
+    openai_data[message.chat.id]['thread'] = client.beta.threads.create()
+    process_in_progress[message.chat.id] = 'ai_question'
+    cancel_btn = types.InlineKeyboardButton(text='🚪 Завершити сесію', callback_data='cancel_ai_question')
+    markup = types.InlineKeyboardMarkup()
+    markup.add(cancel_btn)
+    sent_message = bot.send_message(message.chat.id, '🤖 Сесію зі штучним інтелектом розпочато. Задайте своє питання.',
+                                    reply_markup=markup)
+    openai_data[message.chat.id]['sent_message'] = sent_message
+
+
+@bot.message_handler(
+    func=lambda message: message.text not in button_names and process_in_progress.get(message.chat.id) == 'ai_question')
+@authorized_only(user_type='users')
+def proceed_ai_question(message):
+    with DatabaseConnection() as (conn, cursor):
+        cursor.execute('SELECT name FROM employees WHERE telegram_user_id = %s', (message.from_user.id,))
+        employee_name = cursor.fetchone()[0]
+    employee_name = employee_name.split()[1]
+    thread = openai_data[message.chat.id]['thread']
+    client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role='user',
+        content=message.text
+    )
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant_id,
+        instructions=f'Please address the user as {employee_name} and call him by his name.',
+    )
+    bot.edit_message_reply_markup(message.chat.id, openai_data[message.chat.id]['sent_message'].message_id)
+    sent_message = bot.send_message(message.chat.id, '🔄 Генерація відповіді...')
+    openai_data[message.chat.id]['sent_message'] = sent_message
+    ai_timer = time.time()
+
+    cancel_btn = types.InlineKeyboardButton(text='🚪 Завершити сесію', callback_data='cancel_ai_question')
+    markup = types.InlineKeyboardMarkup()
+    markup.add(cancel_btn)
+
+    while client.beta.threads.runs.retrieve(run_id=run.id, thread_id=thread.id).status != 'completed':
+        if time.time() - ai_timer > 30:
+            bot.edit_message_text('⚠️ Відповідь не знайдена. Спробуйте ще раз.', message.chat.id,
+                                  sent_message.message_id, reply_markup=markup)
+            return
+        sleep(1)
+
+    response = client.beta.threads.messages.list(
+        thread_id=thread.id,
+        limit=1
+    )
+    bot.edit_message_text(response.data[0].content[0].text.value, message.chat.id, sent_message.message_id,
+                          reply_markup=markup, parse_mode='HTML')
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'cancel_ai_question')
+@authorized_only(user_type='users')
+def cancel_ai_question(call):
+    thread = openai_data[call.message.chat.id]['thread']
+    client.beta.threads.delete(thread_id=thread.id)
+    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
+    bot.send_message(call.message.chat.id, '🚪 Сесію зі штучним інтелектом завершено.')
+    del process_in_progress[call.message.chat.id]
+    del openai_data[call.message.chat.id]
 
 
 def send_question_form(message, form_url, delete_previous_message=False):
