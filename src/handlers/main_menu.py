62e0eae6db934bbe1a3053725374f5d22eb486b6
Kyryl Snitsarenko <imithos321@gmail.com>
2025-06-28 18:37:47 +0300

    chore: add law department application button to main menu

--- src/handlers/main_menu.py
+++ src/handlers/main_menu.py
@@ -27,9 +27,14 @@ def send_business_processes(message, edit_message=False):
         text='💡 Бізнес-ініціатива',
         url='https://docs.google.com/forms/d/e/1FAIpQLScJlOaWdUt4wdQZVlUa2PB1c7PXEDdPShJ2bpWhrTmVRqnWQw/viewform'
     )
+    law_department_btn = types.InlineKeyboardButton(
+        text='⚖️ Заявка до юр. департаменту',
+        url='https://docs.google.com/forms/d/1qCFzCNBF3LsX4__-NfCWZsg4o_X2yJ8gkdVKBp1-_k0/edit'
+    )
 
     markup = types.InlineKeyboardMarkup(row_width=1)
-    markup.add(personnel_management_btn, recruitment_btn, office_equipment_btn, hr_btn, business_initiative_btn)
+    markup.add(personnel_management_btn, recruitment_btn, office_equipment_btn, hr_btn, business_initiative_btn,
+               law_department_btn)
     if edit_message:
         bot.edit_message_text('🔍 Оберіть бізнес-процес для перегляду:', message.chat.id, message.message_id,
                               reply_markup=markup)
